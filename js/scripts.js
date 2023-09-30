c67692f7d20b7ebb241982a433e33ed7771b6706
Gloria Treider <gloria.jaf@gmail.com>
2023-09-30 17:28:23 +0200

    Fixing the wheather in navbar

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,8 @@ carouselButton.addEventListener('click', function () {
 
 async function fetchWeather() {
     const apiKey = process.env.OPEN_WEATHER_API_KEY;
-    const city = 'London';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const city = 'Oslo';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;//bonus with metric to get the temprature in Celsius
     try {
         let response = await fetch(url);
         if (!response.ok) {
@@ -45,6 +45,7 @@ fetchWeather();
 
 
     function displayWeather(weatherData) {
+        
         // Extracting the required information from weatherData
         const temperature = `${weatherData.main.temp.toFixed(1)}\u00B0C`; // Adding degree symbol
         const description = weatherData.weather[0].description;
@@ -54,43 +55,17 @@ fetchWeather();
     const weatherIcon = document.createElement('img');
     weatherIcon.src = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-    // Create elements to display temperature and description
-    const temperatureElement = document.createElement('p');
-    temperatureElement.textContent = `Temperature: ${temperature}`;
 
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = `Description: ${description}`;
 
     // Find the appropriate container on the webpage
-    const weatherContainer = document.getElementById('weather-container'); // Replacing 'weather-container' with container ID
-    weatherContainer.appendChild(weatherIcon);
-    weatherContainer.appendChild(temperatureElement);
-    weatherContainer.appendChild(descriptionElement);
+    const weatherIconDOM = document.getElementById('weather-icon'); 
+    weatherIconDOM.appendChild(weatherIcon);
+    
+    const weatherTempDOM = document.getElementById('weather-temp'); 
+    weatherTempDOM.textContent = temperature;
+    //console.log(description);
+    const weatherDescDOM = document.getElementById('weather-description');
+    weatherDescDOM.textContent = description;
 
     }
 
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('javascript connected!');
-
-    // Initialize the carousel
-    const homeCarousel = new bootstrap.Carousel(document.getElementById('homeCarousel'), {
-        interval: 2000,
-        pause: false
-    });
-
-    // Handle pause button click
-    const carouselPause = document.getElementById('carouselPause');
-    carouselPause.addEventListener('click', function () {
-        console.log('pausing the carousel');
-        homeCarousel.pause();
-    });
-
-    // Handle play button click
-    const carouselPlay = document.getElementById('carouselPlay');
-    carouselPlay.addEventListener('click', function () {
-        console.log('cycle the carousel');
-        homeCarousel.cycle();
-    });
-});
-*/
