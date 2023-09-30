353b57ad9f3180e410f8af30da0678961e943b74
Gloria Treider <gloria.jaf@gmail.com>
2023-09-30 15:59:24 +0200

    added weather app fetch data

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,53 @@ carouselButton.addEventListener('click', function () {
     }
 }) 
 
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = 'London';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`); //to catch other server errors
+        }
+        let data = await response.json();
+        console.log(data);
+        displayWeather(data); // Calling the displayWeather function with the JSON data
+        return data;
+    } catch (error) {
+        console.error('There was an error!', error);
+    }
+}
+
+fetchWeather();
+
+
+    function displayWeather(weatherData) {
+        // Extracting the required information from weatherData
+        const temperature = `${weatherData.main.temp.toFixed(1)}\u00B0C`; // Adding degree symbol
+        const description = weatherData.weather[0].description;
+        const iconCode = weatherData.weather[0].icon;
+
+        // Create a new image element
+    const weatherIcon = document.createElement('img');
+    weatherIcon.src = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+    // Create elements to display temperature and description
+    const temperatureElement = document.createElement('p');
+    temperatureElement.textContent = `Temperature: ${temperature}`;
+
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = `Description: ${description}`;
+
+    // Find the appropriate container on the webpage
+    const weatherContainer = document.getElementById('weather-container'); // Replacing 'weather-container' with container ID
+    weatherContainer.appendChild(weatherIcon);
+    weatherContainer.appendChild(temperatureElement);
+    weatherContainer.appendChild(descriptionElement);
+
+    }
+
 /*
 document.addEventListener('DOMContentLoaded', function () {
     console.log('javascript connected!');
